75ce67aac365b1349744f537de85b19a54bccea9
amyangfei <amyangfei@gmail.com>
2018-12-30 00:17:49 +0800

    fix goveralls collected files

--- test.sh
+++ test.sh
@@ -7,7 +7,12 @@ PACKAGES=$(go list ./... | grep -vE 'vendor|examples')
 FILES=$(find . -name "*.go" | grep -vE "vendor|examples")
 
 echo "Running tests..."
-GO111MODULE=on go test -v -cover ${PACKAGES}
+if [ "$GL_TRAVIS_CI" = "on" ] ; then
+    cover_opts="-covermode=count -coverprofile=coverage.out"
+else
+    cover_opts="-cover"
+fi
+GO111MODULE=on go test -v ${cover_opts} ${PACKAGES}
 
 echo "Checking gofmt..."
 gofmt -s -l -w ${FILES} 2>&1 | awk '{print} END{if(NR>0) {exit 1}}'
@@ -22,5 +27,9 @@ GO111MODULE=on go vet -all ${PACKAGES} 2>&1 | awk '{print} END{if(NR>0) {exit 1}
 GO111MODULE=off go get golang.org/x/lint/golint
 echo "Checking golint..."
 golint -set_exit_status ${PACKAGES}
+
+if [ "$GL_TRAVIS_CI" = "on" ] ; then
+    GO111MODULE=on $GOPATH/bin/goveralls -coverprofile=coverage.out -service=travis-ci
+fi
 
 echo "Success"

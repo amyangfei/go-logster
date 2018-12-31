9f85cf57578b94ef99300c38592e48736c6e2d38
amyangfei <amyangfei@gmail.com>
2018-12-31 23:17:37 +0800

    add test case for loadplugin with error

--- logster/helper_test.go
+++ logster/helper_test.go
@@ -2,6 +2,7 @@ package logster
 
 import (
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,14 +10,32 @@ import (
 
 func TestLoadParserPlugin(t *testing.T) {
 	pluginPath := os.Getenv("UT_PARSER_PLUGIN_PATH")
-	t.Log(pluginPath)
 	_, err := LoadParserPlugin(pluginPath)
 	assert.Nil(t, err)
 }
 
 func TestLoadOutputPlugin(t *testing.T) {
 	pluginPath := os.Getenv("UT_OUTPUT_PLUGIN_PATH")
-	t.Log(pluginPath)
 	_, err := LoadOutputPlugin(pluginPath)
 	assert.Nil(t, err)
+}
+
+func TestLoadParserPluginError(t *testing.T) {
+	pluginPath := os.Getenv("UT_OUTPUT_PLUGIN_PATH")
+	_, err := LoadParserPlugin(pluginPath)
+	assert.NotNil(t, err)
+
+	invalidPath := filepath.Join("dummy", pluginPath)
+	_, err = LoadParserPlugin(invalidPath)
+	assert.NotNil(t, err)
+}
+
+func TestLoadOutputPluginError(t *testing.T) {
+	pluginPath := os.Getenv("UT_PARSER_PLUGIN_PATH")
+	_, err := LoadOutputPlugin(pluginPath)
+	assert.NotNil(t, err)
+
+	invalidPath := filepath.Join("dummy", pluginPath)
+	_, err = LoadOutputPlugin(invalidPath)
+	assert.NotNil(t, err)
 }
